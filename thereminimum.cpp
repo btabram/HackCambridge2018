1752d9be97c20f4298994375ab21f28f259c5b29
Tomé Magalhães Gouveia <tomegouveia@Tomes-MacBook-Pro.local>
2018-01-20 23:36:22 +0000

    Supports two hands. Updated output.dat

--- thereminimum.cpp
+++ thereminimum.cpp
@@ -53,16 +53,62 @@ void SampleListener::onFrame(const Controller& controller) {
             << ", gestures: " << frame.gestures().count() << std::endl;
   */
   HandList hands = frame.hands();
-  Hand hand = hands[0];
-  const double pitch = 5.*hand.palmPosition()[1];
-  const double roll = (hand.isLeft())? hand.palmNormal().roll() *RAD_TO_DEG : -1.*hand.palmNormal().roll() *RAD_TO_DEG;
-  
-  double vol = (roll + 90)/180;
-  vol = ( vol > 1) ? 1 : vol ;
-  vol = ( vol < 0) ? 0 : vol ;
 
-  if (!frame.hands().isEmpty()){
-    std::cout << "(" << pitch << ", " << vol << ")" << std::endl;
+  if (hands.count()>2)
+  {
+    std::cout << "More than two hands. Nope." << std::endl;
+    exit(123);
+  } 
+
+  if (hands.count()==1)
+  {
+    Hand hand = hands[0];
+    const double pitch = 5.*hand.palmPosition()[1];
+    const double roll = (hand.isLeft())? hand.palmNormal().roll() *RAD_TO_DEG : -1.*hand.palmNormal().roll() *RAD_TO_DEG;
+    
+    double vol = (roll + 90)/180;
+    vol = ( vol > 1) ? 1 : vol ;
+    vol = ( vol < 0) ? 0 : vol ;
+
+    if (!frame.hands().isEmpty()){
+      std::cout << "(" << pitch << ", " << vol << ")" << std::endl;
+    }
+  }
+  else if (hands.count()==2)
+  {
+    Hand handL, handR;
+    if ((hands[0].isLeft() && hands[1].isLeft()) || (hands[0].isRight() && hands[1].isRight()))
+    {
+      std::cout << "Both hands of the same sign. Nope." << std::endl;
+      exit(123);
+    } else if (hands[0].isLeft() && hands[1].isRight())
+    {
+      handL=hands[0];
+      handR=hands[1];
+    } else if (hands[1].isLeft() && hands[0].isRight())
+    {
+      handR=hands[0];
+      handL=hands[1];
+    }
+  
+    const double pitchL = 5.*handL.palmPosition()[1];
+    const double rollL = handL.palmNormal().roll() *RAD_TO_DEG ;
+    
+    double volL = (rollL + 90)/180;
+    volL = ( volL > 1) ? 1 : volL ;
+    volL = ( volL < 0) ? 0 : volL ;
+  
+    const double pitchR = 5.*handR.palmPosition()[1];
+    const double rollR = -1.*handR.palmNormal().roll() *RAD_TO_DEG;
+    
+    double volR = (rollR + 90)/180;
+    volR = ( volR > 1) ? 1 : volR ;
+    volR = ( volR < 0) ? 0 : volR ;
+  
+  
+    if (!hands.isEmpty()){
+        std::cout << "(" << pitchL << ", " << volL << ", " << pitchR << ", " << volR << ")" << std::endl;
+    }
   }
 
 }
