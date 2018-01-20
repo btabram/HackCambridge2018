71af51487f027c81bae889127a0c428f1fe09ab5
Tomé Magalhães Gouveia <tomegouveia@Tomes-MacBook-Pro.local>
2018-01-20 18:25:44 +0000

    initial push

---
+++ thereminimum.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <cstring>
+#include "Leap.h"
+
+using namespace Leap;
+
+class SampleListener : public Listener {
+  public:
+    virtual void onInit(const Controller&);
+    virtual void onConnect(const Controller&);
+    virtual void onDisconnect(const Controller&);
+    virtual void onExit(const Controller&);
+    virtual void onFrame(const Controller&);
+
+  private:
+};
+
+const std::string fingerNames[] = {"Thumb", "Index", "Middle", "Ring", "Pinky"};
+const std::string boneNames[] = {"Metacarpal", "Proximal", "Middle", "Distal"};
+const std::string stateNames[] = {"STATE_INVALID", "STATE_START", "STATE_UPDATE", "STATE_END"};
+
+void SampleListener::onInit(const Controller& controller) {
+  std::cout << "Initialized" << std::endl;
+}
+
+void SampleListener::onConnect(const Controller& controller) {
+  std::cout << "Connected" << std::endl;
+  controller.enableGesture(Gesture::TYPE_CIRCLE);
+  controller.enableGesture(Gesture::TYPE_KEY_TAP);
+  controller.enableGesture(Gesture::TYPE_SCREEN_TAP);
+  controller.enableGesture(Gesture::TYPE_SWIPE);
+}
+
+void SampleListener::onDisconnect(const Controller& controller) {
+  // Note: not dispatched when running in a debugger.
+  std::cout << "Disconnected" << std::endl;
+}
+
+void SampleListener::onExit(const Controller& controller) {
+  std::cout << "Exited" << std::endl;
+}
+
+void SampleListener::onFrame(const Controller& controller) {
+  // Get the most recent frame and report some basic information
+  
+  const Frame frame = controller.frame();
+  /*
+  std::cout << "Frame id: " << frame.id()
+            << ", timestamp: " << frame.timestamp()
+            << ", hands: " << frame.hands().count()
+            << ", extended fingers: " << frame.fingers().extended().count()
+            << ", tools: " << frame.tools().count()
+            << ", gestures: " << frame.gestures().count() << std::endl;
+  */
+  HandList hands = frame.hands();
+  for (HandList::const_iterator hl = hands.begin(); hl != hands.end(); ++hl) {
+    // Get the first hand
+    const Hand hand = *hl;
+    std::cout << std::string(2, ' ') << hand.palmPosition().x() << std::endl;
+  }
+
+  if (!frame.hands().isEmpty()) {
+    std::cout << std::endl;
+  }
+
+}
+int main(int argc, char** argv) {
+
+  // Keep this process running until Enter is pressed
+  std::cout << "Press Enter to quit..." << std::endl;
+  std::cin.get();
+
+  Controller controller;
+  SampleListener listener;
+
+  const Frame frame = controller.frame();
+
+  controller.addListener(listener);
+  HandList hands = frame.hands();
+   for (HandList::const_iterator hl = hands.begin(); hl != hands.end(); ++hl) {
+    // Get the first hand
+    const Hand hand = *hl;
+    std::string handType = hand.isLeft() ? "Left hand" : "Right hand";
+    std::cout << std::string(2, ' ') << handType << ", id: " << hand.id()
+              << ", palm position: " << hand.palmPosition() << std::endl;
+    // Get the hand's normal vector and direction
+    const Vector normal = hand.palmNormal();
+    const Vector direction = hand.direction();
+
+     std::cout << std::string(2, ' ') << "Palm normal" << normal << std::endl;
+    // Get fingers
+    const FingerList fingers = hand.fingers();
+    for (FingerList::const_iterator fl = fingers.begin(); fl != fingers.end(); ++fl) {
+      const Finger finger = *fl;
+      std::cout << std::string(4, ' ') <<  fingerNames[finger.type()]
+                << " finger, id: " << finger.id()
+                << ", length: " << finger.length()
+                << "mm, width: " << finger.width() << std::endl;
+    }
+  }
+
+
+
+  if (argc > 1 && strcmp(argv[1], "--bg") == 0)
+    controller.setPolicy(Leap::Controller::POLICY_BACKGROUND_FRAMES);
+  
+  
+  std::cout << "Press Enter to quit..." << std::endl;
+  std::cin.get();
+
+  controller.removeListener(listener);
+  
+  return 0;
+}
