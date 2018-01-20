d71ed6a84dd7d1fa376e291bfab050ad24c26845
Will Grant <wpg23@cam.ac.uk>
2018-01-20 20:48:31 +0000

    Roughly working version of palmpositiontosound

--- palmPositionToSound.py
+++ palmPositionToSound.py
@@ -8,12 +8,12 @@ import wave, struct, math
 
 duration = 1.0       # seconds
 frequency = 1040.0    # hertz
-rFreq = 1760.00  # A
+rFreq = 760.00  # A
 lFreq =  523.25  # C
 
 
 
-sampleRate = 44100 # hertz
+sampleRate = 2000 # hertz
 print(sampleRate)
 # instantiate PyAudio (1)
 p = pyaudio.PyAudio()
@@ -27,7 +27,7 @@ stream = p.open(format=p.get_format_from_width(2),
 # read data
 # data = wf.readframes(CHUNK)
 
-
+i = 0
 while True:
     try:
         palmPosition = input().strip()
@@ -37,17 +37,24 @@ while True:
         continue
     palmPosition = palmPosition.strip("()")
     try:
-        x, y, z = [float(x) for x in palmPosition.split(",")]
+        x, y, z = [5* float(x) for x in palmPosition.split(",")]
+        for j in range(35):
+            i+= 1
+            l = int(32767.0*math.cos(y*math.pi*float(i)/float(sampleRate)))
+            r = int(32767.0*math.cos(y*math.pi*float(i)/float(sampleRate)))
+            data = struct.pack('<hh', l, r )
+            stream.write(data)
     except ValueError:
         continue
     print(x,y,z)
-for i in range(int(duration * sampleRate)):
+
+# for i in range(int(duration * sampleRate)):
     # l = int(32767.0*math.cos(lFreq*math.pi*float(i)/float(sampleRate)))
     # r = int(32767.0*math.cos(rFreq*math.pi*float(i)/float(sampleRate)))
-    l = int(32767.0*math.cos(x *math.pi*float(i)/float(sampleRate)))
-    r = int(32767.0*math.cos(y *math.pi*float(i)/float(sampleRate)))
-    data = struct.pack('<hh', l, r )
-    stream.write(data)
+    # l = int(32767.0*math.cos(x *math.pi*float(i)/float(sampleRate)))
+    # r = int(32767.0*math.cos(y *math.pi*float(i)/float(sampleRate)))
+    # data = struct.pack('<hh', l, r )
+    # stream.write(data)
 
 # stop stream (4)
 stream.stop_stream()
