86bf95c11a9f90a0927c13c668e8198d82b50ebd
Tomé Magalhães Gouveia <tomegouveia@Tomes-MacBook-Pro.local>
2018-01-21 11:30:57 +0000

    Pitch

--- thereminimum.py
+++ thereminimum.py
@@ -53,7 +53,7 @@ class SampleListener(Leap.Listener):
                 if vol < 0:
                     vol = 0
 
-                pitch = 660 + 7.5 * max(hand.palm_position[1] - 100, 0)
+                pitch = 440 + 15.6*max(hand.palm_position[1]-10,0) 
 
                 #print pitch, vol
                 q.put(ppts.LeapData(pitch, vol))
@@ -78,8 +78,8 @@ class SampleListener(Leap.Listener):
                 if volR < 0:
                     volR = 0
 
-                pitchL = 660 + 7.5 * max(handL.palm_position[1] - 100, 0)
-                pitchR = 660 + 7.5 * max(handR.palm_position[1] - 100, 0)
+                pitchL = 440 + 15.6 * max(handL.palm_position[1] - 10, 0)
+                pitchR = 440 + 15.6 * max(handR.palm_position[1] - 10, 0)
 
                 q.put(ppts.LeapData(pitchL, volL, pitchR, volR))
                 #print pitchL, volL, pitchR, volR
